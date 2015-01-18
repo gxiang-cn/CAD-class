efb90a7892b7010d799513ea8069def238e46dbf
Xiang Gao <polarisgx@hotmail.com>
2015-01-18 18:32:22 +0800

    finish graph setup function

--- graph.cpp
+++ graph.cpp
@@ -13,14 +13,14 @@ const int NONE = -1;
 class Edge
 {
 public:
-    Edge():mVertexIndex(NONE);
-    Edge(int AdjVertex) { mVertexIndex = AdjVertex; }; //TODO Can we merge two constructor together? 
-    ~Edge(); 
+    Edge():mVertexIndex(NONE){};
+    Edge(int AdjVertex) { mVertexIndex = AdjVertex; } //TODO Can we merge two constructor together? 
+	~Edge() {}
 
-    const int adjVertex() {return mVertexIndex;};
+    int adjVertex() const {return mVertexIndex;};
 private:
     int mVertexIndex;
-}
+};
 
 
 class Vertex
@@ -28,32 +28,34 @@ class Vertex
     friend class Graph;
 
 public:
+    Vertex() {};
     Vertex(const std::string& name) {mName = name;}
-    ~Vertex();
+	~Vertex() {};
 
-    const std::string& name();
+	const std::string& name() const { return mName; }
     void addEdge(int v);
 
 private:
     std::string mName;
-    std::List<Edge> mEdgeList;
+    std::list<Edge> mEdgeList;
 };
 
 
-typedef std::list<Vertex> VertexList;
+
 
 //Adjecent list
 class Graph
 {
 public:
-    Graph();
-    ~Graph();
+    Graph() {};
+    ~Graph(){};
 
-    bool addVertex(const std::string& name);
+    int addVertex(const std::string& name);
     bool addEdge(const std::string& name1, const std::string& name2);
-    void dfs();
-    void bfs();
-    void print();
+    //void dfs() const;
+    //void bfs() const;
+
+    void print(void) const;
 private:
     std::vector<Vertex> mAList;
 };
@@ -62,7 +64,7 @@ private:
 //Implementation
 //-----------------------------------------------------------------------------
 
-using std;
+using namespace std;
 
 //-----------------------------------------------------------------------------
 // Class Vertex
@@ -71,7 +73,7 @@ void Vertex::addEdge(int v)
 {
     if (v < 0) return;
     if (!mEdgeList.empty()) {
-	for (list::const_iterator i = mEdgeList.begin(); i < mEdgeList.end();
+	for (list<Edge>::const_iterator i = mEdgeList.begin(); i != mEdgeList.end();
 		i++) {
 	    if (i->adjVertex() == v) {
 		cout << "Edge to " << v << "exists" << endl;
@@ -88,24 +90,24 @@ void Vertex::addEdge(int v)
 // Class Graph
 //-----------------------------------------------------------------------------
 
-bool Graph::addVertex(const string& name)
+int Graph::addVertex(const string& name)
 {
     if (name.empty()) {
 	cout << "Vertex name is empty." << endl;
-	return false;
+	return NONE;
     }
     if (!mAList.empty()) {
-	for (vector::iterator i = mAList.begin(); i < mAList.end(); i++) {
-	    if (name == i->name()) {
-		cout << "Vertex" << name << "is existing" << endl;
-		return false;
+	    for (vector<Vertex>::const_iterator i = mAList.begin(); i < mAList.end(); i++) {
+	        if (name == i->name()) {
+		        cout << "Vertex " << name << " is existing" << endl;
+		        return i-mAList.begin();
+	        }
 	    }
-	}
     }
 
     Vertex v(name);
     mAList.push_back(v);
-    return true;
+    return mAList.size() - 1;
 }
 
 bool Graph::addEdge(const string& name1, const string& name2)
@@ -122,56 +124,65 @@ bool Graph::addEdge(const string& name1, const string& name2)
 
     int v1 = NONE;
     int v2 = NONE;
-    for (int i = 0; i < mAList.size(); i++) {
-	if (v1 != NONE) {
+    for (unsigned int i = 0; i < mAList.size(); i++) {
+	if (v1 == NONE) {
 	    if (name1 == mAList[i].name())  v1 = i; 
 	}
-	if (v2 != NONE) {
+	if (v2 == NONE) {
 	    if (name2 == mAList[i].name())  v2 = i;
 	}
 
-	if (v1 != NONE && v2 != NONE) {
-	    mAList[v1].addEdge(v2)
-	    return true;
-	} 
+	if (v1 != NONE && v2 != NONE) break;
+
     }
-    if (v1 == NONE) addVertex(name1);
-    if (v2 == NONE) addVertex(name2);
+    if (v1 == NONE) v1 = addVertex(name1);
+    if (v2 == NONE) v2 = addVertex(name2);
+	
     mAList[v1].addEdge(v2);
+	mAList[v2].addEdge(v1);
     return true;
 }
 
 //TODO add overload for stream?
-void Graph::print()
+
+void Graph::print(void) const
 {
     cout << "Graph" << endl;
-    for (vector::const_iterator i = mAList.begin(); i < mAList.end(); i++) {
+
+    for (vector<Vertex>::const_iterator i = mAList.cbegin(); i < mAList.cend(); i++) {
 	cout << i->name() << "\t" ;
 	//Use direct access to Vertex. TODO add iterator.
-	for (vecotr::const_iterator j = i->mEdgeList.begin(); 
-		j < i->mEdgeList.end(); j++) {
+	
+	for (list<Edge>::const_iterator j = i->mEdgeList.begin(); 
+		j != i->mEdgeList.end(); j++) {
 	    cout << mAList[j->adjVertex()].name() << "\t";
 	}
+	
 	cout << endl;
     }
+	return;
 }
 
 } //end of namespace CAD
 
+using namespace CAD;
 Graph& createGraph(Graph& g)
 {
     g.addEdge("A", "B");
     g.addEdge("A", "C");
+	g.addEdge("B", "C");
+	g.addEdge("B", "D");
+	g.addEdge("B", "E");
     g.addEdge("C", "D");
-    g.addEdge("B", "E");
     g.addEdge("C", "E");
+	return g;
     
 }
 
 int main()
 {
 
-    Graph g;
+    CAD::Graph g;
     g = createGraph(g);
     g.print();
 
